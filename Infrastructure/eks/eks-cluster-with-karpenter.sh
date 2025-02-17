04b3469f9c03e266b5368ca05f0d7f68956a9c61
bnahuy <73353844+bnahuy@users.noreply.github.com>
2025-02-18 00:34:11 +0700

    Update eks-cluster-with-karpenter.sh

--- Infrastructure/eks/eks-cluster-with-karpenter.sh
+++ Infrastructure/eks/eks-cluster-with-karpenter.sh
@@ -4,7 +4,7 @@ K8S_VERSION="1.32"
 
 AWS_PARTITION="aws" # if you are not using standard partitions, you may need to configure to aws-cn / aws-us-gov
 CLUSTER_NAME="lab-eks-cluster"
-AWS_DEFAULT_REGION="us-west-2"
+AWS_DEFAULT_REGION="us-west-1"
 AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
 TEMPOUT="$(mktemp)"
 ARM_AMI_ID="$(aws ssm get-parameter --name /aws/service/eks/optimized-ami/${K8S_VERSION}/amazon-linux-2-arm64/recommended/image_id --query Parameter.Value --output text)"
@@ -12,6 +12,13 @@ AMD_AMI_ID="$(aws ssm get-parameter --name /aws/service/eks/optimized-ami/${K8S_
 GPU_AMI_ID="$(aws ssm get-parameter --name /aws/service/eks/optimized-ami/${K8S_VERSION}/amazon-linux-2-gpu/recommended/image_id --query Parameter.Value --output text)"
 
 echo "${KARPENTER_NAMESPACE}" "${KARPENTER_VERSION}" "${K8S_VERSION}" "${CLUSTER_NAME}" "${AWS_DEFAULT_REGION}" "${AWS_ACCOUNT_ID}" "${TEMPOUT}" "${ARM_AMI_ID}" "${AMD_AMI_ID}" "${GPU_AMI_ID}"
+
+curl -fsSL https://raw.githubusercontent.com/aws/karpenter-provider-aws/v"${KARPENTER_VERSION}"/website/content/en/preview/getting-started/getting-started-with-karpenter/cloudformation.yaml  > "${TEMPOUT}" \
+&& aws cloudformation deploy \
+  --stack-name "Karpenter-${CLUSTER_NAME}" \
+  --template-file "${TEMPOUT}" \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides "ClusterName=${CLUSTER_NAME}"
 
 echo "🔹 Đang tìm VPC có tag env=lab..."
 VPC_ID=$(aws ec2 describe-vpcs \
