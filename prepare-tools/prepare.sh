76b79bf26635e32f4b10c22fea73991b1d2907b5
bnahuy <73353844+bnahuy@users.noreply.github.com>
2025-02-19 15:32:24 +0700

    Update prepare.sh

--- prepare-tools/prepare.sh
+++ prepare-tools/prepare.sh
@@ -1,3 +1,7 @@
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
 # Install eksctl
 # for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
 ARCH=amd64
