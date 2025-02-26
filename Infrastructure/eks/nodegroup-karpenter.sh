bd29f23e29b5463982d3c9deda67c421dc1f98d2
bnahuy <73353844+bnahuy@users.noreply.github.com>
2025-02-27 01:29:39 +0700

    Update nodegroup-karpenter.sh

--- Infrastructure/eks/nodegroup-karpenter.sh
+++ Infrastructure/eks/nodegroup-karpenter.sh
@@ -11,6 +11,6 @@ AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
 TEMPOUT="$(mktemp)"
 ARCHITECTURE="arm64"
 
+echo "Bắt đầu lấy AMI..."
 BOTTLEROCKET_AMI="$(aws ssm get-parameter --name /aws/service/bottlerocket/aws-k8s-${K8S_VERSION}/${ARCHITECTURE}/latest/image_id --region ${AWS_DEFAULT_REGION} --query "Parameter.Value" --output text)"
-
-echo ${BOTTLEROCKET_AMI}
+echo "AMI: ${BOTTLEROCKET_AMI}"
