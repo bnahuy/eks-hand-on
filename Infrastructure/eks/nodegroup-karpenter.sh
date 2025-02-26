b510ad010517aadcedaf6e3c7cbd386e32f719df
bnahuy <73353844+bnahuy@users.noreply.github.com>
2025-02-27 01:18:48 +0700

    Update nodegroup-karpenter.sh

--- Infrastructure/eks/nodegroup-karpenter.sh
+++ Infrastructure/eks/nodegroup-karpenter.sh
@@ -1,1 +1,13 @@
-adasd
+#!/bin/bash
+
+KARPENTER_NAMESPACE="kube-system"
+KARPENTER_VERSION="1.2.1"
+K8S_VERSION="1.32"
+
+AWS_PARTITION="aws"
+CLUSTER_NAME="lab-eks-cluster"
+AWS_DEFAULT_REGION="us-east-1"
+AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
+TEMPOUT="$(mktemp)"
+
+aws ssm get-parameter --name /aws/service/bottlerocket/aws-k8s-kubernetes-version-flavor/architecture/latest/image_id --region region-code --query "Parameter.Value" --output text
